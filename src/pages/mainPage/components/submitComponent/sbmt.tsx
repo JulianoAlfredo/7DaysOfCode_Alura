d392dff67b87f00881e6cfc704236e05087b038d
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2022-04-10 13:08:46 -0300

    :poop:Finish the first day, but not responsive

--- src/pages/mainPage/components/submitComponent/sbmt.tsx
+++ src/pages/mainPage/components/submitComponent/sbmt.tsx
@@ -2,15 +2,25 @@ import React from "react";
 
 import './sbmtCss.css'
 import treeImage from '../../../../img/treeImage.svg'
+import emailVector from '../../../../img/emailvector.svg'
+
 export default function SubmitComponent(){
     return(
         <div className='main-sbmt-component'>
             <div className="text-div">
-                <p>LOREM</p>
+                <p>Sua casa com as</p>
+                <h1>melhores</h1>
+                <h1>plantas</h1>
+                <p>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
+                <div className="button-input-submit">
+                    <input placeholder="Insira seu email"></input>
+                    <button>Assinar newsletter</button>
+                </div>
             </div>
             <div className='image-div'>
                 <img src={treeImage} />
             </div>
+            
         </div>
     )
 }
--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import MenuComponent from "./components/menuComponent/menu";
 import SubmitComponent from "./components/submitComponent/sbmt";
+import svgBackground from "../../img/Vector.svg"
+import "./main.css"
 export default function MainPage(){
     return(
         <div className="mainPage">
+            
+            <div className="imgBackGround">
+                <img  alt="VECTOR" src={svgBackground} />
+            </div>
             
             <div className="menu">
                 <MenuComponent />
              </div>
 
-             <div>
+             <div className="sbmt">
                  <SubmitComponent />
              </div>
+
         </div>
         
     )
